2a12c8cf2f7c530ea908f11c58ca57ba2910e88d
Selam Mulugeta <selammulugeta994@gmail.com>
2025-08-27 13:13:50 +0300

    Create a User Profile Card Using Props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
-
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         <Header />
-        <WelcomeMessage /> 
+        <WelcomeMessage />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <MainContent />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -38,10 +38,8 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <Footer />
-
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
