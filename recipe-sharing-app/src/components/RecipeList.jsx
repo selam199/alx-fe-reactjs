91858180dce8b77ac5a5d2c510da9da04a5ca2af
Selam Mulugeta <selammulugeta994@gmail.com>
2025-09-13 14:24:46 +0300

    Initialize the Recipe Sharing Application with Zustand State Management

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,4 @@
-import useRecipeStore from '../store/useRecipeStore';
+import useRecipeStore from '../store/recipeStore';
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
