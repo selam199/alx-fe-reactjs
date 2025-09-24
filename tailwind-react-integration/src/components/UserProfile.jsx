7e0014ce4cacf7d652789fed18073e59c41ed4c0
Selam Mulugeta <selammulugeta994@gmail.com>
2025-09-24 13:14:24 +0300

    Responsive Design Implementation Using Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,18 +1,26 @@
-function UserProfile() {
+import React from "react";
+
+const UserProfile = () => {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-10 md:my-20 rounded-lg shadow-lg text-center">
       <img
-        className="w-36 h-36 rounded-full mx-auto"
+        className="w-24 h-24 md:w-36 md:h-36 rounded-full mx-auto"
         src="https://via.placeholder.com/150"
-        alt="User"
+        alt="User Avatar"
       />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-base text-gray-600">
-        Developer at Example Co. Loves to write code and explore new
+      <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-4">
+        John Doe
+      </h1>
+      <p className="text-sm sm:text-sm md:text-base text-gray-600">
+         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
     </div>
   );
-}
+};
 
 export default UserProfile;
+
+
+
+ 
